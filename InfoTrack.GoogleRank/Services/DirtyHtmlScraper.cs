e703388ac9a0592c5a0af29812e3bb06e45e0475
Robert Snyder <immeraufdemhund@hotmail.com>
2022-09-01 10:49:23 -0500

    finish implementing dirty html scraper.
    turns out the html file I was using to parse originally wasn't going to be what I was going to see.
    I'll keep it around if in the future the results are returned in a similar fashion

--- InfoTrack.GoogleRank/Services/DirtyHtmlScraper.cs
+++ InfoTrack.GoogleRank/Services/DirtyHtmlScraper.cs
@@ -1,74 +1,122 @@
-﻿using System.Text;
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 
 namespace InfoTrack.GoogleRank.Services;
 
 public class DirtyHtmlScraper : IHtmlScraper
 {
-    private delegate IndexedUrl[] CssSelector(TextReader reader, ILogger logger);
-    public Task<IndexedUrl[]> SelectElementsWithCssSelector(Stream stream, ILogger logger)
+    public async Task<IndexedUrl[]> SelectElementsWithCssSelector(Stream stream, ILogger logger)
     {
         using var reader = new StreamReader(stream);
-
-        SkipUntilDivId_search(reader);
-        var depth = 0;
-        while (true)
+        var text = await reader.ReadToEndAsync();
+        var matches = ChunkHtmlIntoSpecificDivsFromHtmlString(text).ToArray();
+        if (matches.Length > 0)
         {
-            var c = GuardAgainstEndOfStream(reader);
-            if (c == '<')
-            {
-                var element = ReadToEndOfElement(reader, "<");
-                if (element.StartsWith("<div"))
-                {
-                    depth++;
-                }
+            logger.LogDebug("found {Count} result divs", matches.Length);
+        }
+        else
+        {
+            logger.LogWarning("Did not find any search result divs to return results with");
+            return Array.Empty<IndexedUrl>();
+        }
 
-                if (element.StartsWith("</div>"))
+        try
+        {
+            return matches
+                .Select(resultElement => resultElement.Single(x => x.Value.Contains(GetSearchStringFromCssSelector(".BNeawe.UPmit.AP7Wnd"))))
+                .Select(urlHeaderElement =>
                 {
-                    depth--;
-                }
-            }
+                    var endOfOpeningDivTagIndex = text.IndexOf(">", urlHeaderElement.Index);
+                    var startOfClosingDivTag = text.IndexOf("</div>", endOfOpeningDivTagIndex);
+                    var length = startOfClosingDivTag - endOfOpeningDivTagIndex;
+                    return text.Substring(endOfOpeningDivTagIndex + 1, length - 1);
+                })
+                .Select(headerText => headerText.Split(' ', StringSplitOptions.RemoveEmptyEntries)[0].Trim())
+                .Select((x, index) => new IndexedUrl(index, x))
+                .ToArray();
+        }
+        catch (InvalidOperationException e)
+        {
+            logger.LogError(e, "my css search string for where the results are must be wrong");
+            return Array.Empty<IndexedUrl>();
         }
     }
 
-    private static string GetId(string element) => Regex.Match(element, @" id=[""'](?'id'.+?)[""']").Groups["id"].Value;
-    private static void SkipUntilDivId_search(TextReader s)
+    private IEnumerable<List<Group>> ChunkHtmlIntoSpecificDivsFromHtmlString(string html)
     {
-        while (true)
+        var all = Regex.Matches(html, @"<(.*?)>", RegexOptions.Multiline);
+        var selector = GetSearchStringFromCssSelector(".Gx5Zad.fP1Qef.xpd.EtOod.pkphOe");
+
+        var enumerator = all.Select(a => a.Groups[0]).GetEnumerator();
+        while (enumerator.MoveNext())
         {
-            var c = GuardAgainstEndOfStream(s);
-            if (c == '<')
+            var currentGroupToCheck = enumerator.Current;
+            if (!currentGroupToCheck.Value.Contains(selector)) continue;
+
+            yield return GetCurrentElementAndAllChildren(enumerator);
+        }
+    }
+
+    private static List<Group> GetCurrentElementAndAllChildren(IEnumerator<Group> enumerator)
+    {
+        var newListToReturn = new List<Group>();
+        newListToReturn.Add(enumerator.Current);
+        var depth = 1;
+        while (depth != 0)
+        {
+            enumerator.MoveNext();
+            newListToReturn.Add(enumerator.Current);
+
+            var value = enumerator.Current.Value;
+            var isClosingTag = value.StartsWith("</");
+            if (isClosingTag)
             {
-                var element = ReadToEndOfElement(s, "<");
-                if (element.StartsWith("<div"))
+                depth--;
+                if (depth == 0)
                 {
-                    if (GetId(element).Equals("search"))
-                    {
-                        break;
-                    }
+                    break;
                 }
             }
-        }
-    }
-
-    private static string ReadToEndOfElement(TextReader s, string elementStart)
-    {
-        var sb = new StringBuilder(elementStart);
-        while (true)
-        {
-            var c = GuardAgainstEndOfStream(s);
-            sb.Append(c);
-            if (c == '>')
+            else
             {
-                return sb.ToString();
+                // skip empty tags
+                if (IsEmptyTag(value)) continue;
+
+                depth++;
             }
         }
+
+        return newListToReturn;
     }
 
-    private static char GuardAgainstEndOfStream(TextReader s)
+    private static bool IsEmptyTag(string element)
     {
-        var value = s.Read();
-        if (value < 1) throw new Exception("Unexpected End Of Stream condition");
-        return Convert.ToChar(value);
+        var match = Regex.Match(element, @"<(?'tagname'\w+).*>");
+        if (!match.Success) return false;
+
+        var tagName = match.Groups["tagname"].Value;
+        return emptyTags.Contains(tagName);
+    }
+
+    // https://developer.mozilla.org/en-US/docs/Glossary/Empty_element
+    private static readonly string[] emptyTags = new string[]
+    {
+        "area", "br", "col", "embed", "hr", "img",
+        "input", "keygen", "link", "meta", "param", "source", "track", "wbr"
+    };
+
+    private static string GetSearchStringFromCssSelector(string cssSelector)
+    {
+        if (cssSelector.Contains(" "))
+            throw new Exception("can't handle descendants yet");
+
+        var idMatch = Regex.Match(cssSelector, @"#(?'id'\w+)");
+        if (idMatch.Success)
+        {
+            return $"id=\"{idMatch.Groups["id"].Value}\"";
+        }
+
+        var classMatches = Regex.Matches(cssSelector, @".(?'className'\w+)");
+        var classNames = string.Join(" ", classMatches.Select(m => m.Groups["className"].Value));
+        return $"class=\"{classNames}\"";
     }
 }
