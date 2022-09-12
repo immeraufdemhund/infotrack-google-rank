54723ce4f9c3bc1aabaa039034339e2f18750f5d
Robert Snyder <immeraufdemhund@hotmail.com>
2022-09-12 08:44:03 -0500

    **WIP**
    add template data as a placeholder to change backend to add more information from the google search results.

--- InfoTrack.GoogleRank/ClientApp/src/components/Home.js
+++ InfoTrack.GoogleRank/ClientApp/src/components/Home.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react';
 import {IndexedUrlTableComponent} from "./IndexedUrlTableComponent";
 
 export function Home(props) {
-    const [state, setState] = useState({googleRankResult: [], loading: true});
+    const [state, setState] = useState({googleRankResult: {data: [], summary: ''}, loading: true});
 
     async function populateGoogleRank() {
         const response = await fetch('GoogleRank');
@@ -11,18 +11,30 @@ export function Home(props) {
 
     useEffect(() => {
         populateGoogleRank()
-            .then((data) => {
-                setState({googleRankResult: data, loading: false});
+            .then((response) => {
+                setState({
+                    googleRankResult: {
+                        data: response.data.map(x => {
+                            return {
+                                index: x.index + 1,
+                                url: x.url.replace("www.", ""),
+                                title: x.title ?? 'Fake title',
+                                clickableLink: x.fullUrl ?? 'https://google.com/search?num=100&q=something',
+                                description: x.description ?? '<span class="r0bn4c rQMQod">Jan 29, 2021</span>By Comparison...'
+                        }}),
+                        summary: response.summary
+                    },
+                    loading: false
+                });
             });
     }, []);
 
     return (
         <div>
-            <h1 id="tabelLabel">Google Results</h1>
-            <p>This component demonstrates fetching data from the server.</p>
+            <h1 id="tableLabel">Google Results</h1>
             {state.loading
                 ? <p><em>Loading...</em></p>
-                : <IndexedUrlTableComponent googleRankResult={state.googleRankResult}/>}
+                : <IndexedUrlTableComponent data={state.googleRankResult.data} summary={state.googleRankResult.summary}/>}
         </div>
     );
 }
